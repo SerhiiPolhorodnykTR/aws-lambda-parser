41fb9dfc9d18da58f64b6f2116d89f49fb120eb4
Serhii Polhorodnyk <serhii.polhorodnyk@thomsonreuters.com>
2019-11-07 16:38:49 +0200

    tags and marker checks are out to functions

--- aws-lambda-parser.py
+++ aws-lambda-parser.py
@@ -1,12 +1,11 @@
 import json
 import boto3
 
-list = []
+lambda_list = []
 client = boto3.client('lambda')
 
 
 def next_page(marker):
-    client = boto3.client('lambda')
     return client.list_functions(Marker=marker)
 
 
@@ -15,32 +14,34 @@ def parse_page(page):
     for n in range(num):
         if page['Functions'][n]['Runtime'] == "python2.7":
             arn = page['Functions'][n]['FunctionArn']
-            tags = client.list_tags(Resource=arn)
-            try:
-                tag = tags['Tags']['tr:resource-owner']
-            except:
-                tag = ''
+            owner_tag = test_owner_tag(client.list_tags(Resource=arn))
             line = page['Functions'][n]['FunctionName'] + ' Description: ' + page['Functions'][n][
-                'Description'] + " LastModified: " + page['Functions'][n]['LastModified'] + " Owner: " + tag
-            list.append(line)
-            # print(str)
+                'Description'] + " LastModified: " + page['Functions'][n]['LastModified'] + " Owner: " + owner_tag
+            lambda_list.append(line)
+
+
+def test_marker(result):
+    try:
+        return result["NextMarker"]
+    except Exception:
+        return ''
+
+
+def test_owner_tag(result):
+    try:
+        return result['Tags']['tr:resource-owner']
+    except Exception:
+        return ''
 
 
 def lambda_handler(event, context):
     result = client.list_functions()
-    num = len(result['Functions'])
-    try:
-        marker = result["NextMarker"]
-    except:
-        marker = ''
+    marker = test_marker(result)
     parse_page(result)
 
     while marker:
         page = next_page(marker)
-        try:
-            marker = page["NextMarker"]
-        except:
-            marker = ''
+        marker = test_marker(result)
         parse_page(page)
 
-    return (list)
+    return lambda_list
